9660224c771615b5a89485c799aba72a697c5344
Алексей Селиверстов <aleshaseliverstov00@gmail.com>
2026-06-28 12:56:15 +0500

    добвление стандартов

--- lesson_3_task_3.py
+++ lesson_3_task_3.py
@@ -1,14 +1,11 @@
 from address import Address
 from mailing import Mailing
 
-# Создаём адреса
 to_addr = Address("123456", "Москва", "Тверская", "15", "7")
 from_addr = Address("654321", "Санкт-Петербург", "Невский", "22", "12")
 
-# Создаём отправление
 mailing = Mailing(to_addr, from_addr, 350.0, "TRK123456789")
 
-# Вывод в требуемом формате
 print(
     f"Отправление {mailing.track} из "
     f"{mailing.from_address.index}, {mailing.from_address.city}, "
